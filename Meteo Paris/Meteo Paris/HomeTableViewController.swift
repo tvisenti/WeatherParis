6c79be029afa23c4b527c2b603783c7e30c43238
tvisenti <tvisenti@student.42.fr>
2017-08-17 09:30:00 +0200

    [First Push]

---
+++ Meteo Paris/Meteo Paris/HomeTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeTableViewController.swift
+//  Meteo Paris
+//
+//  Created by Thomas VISENTIN on 8/17/17.
+//  Copyright © 2017 Thomas VISENTIN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+    
+    @IBOutlet weak var titleNavigationItem: UINavigationItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
