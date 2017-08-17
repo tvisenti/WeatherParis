5590ddb655df64f6bc1048b661dff402be3216ea
tvisenti <tvisenti@student.42.fr>
2017-08-17 09:47:24 +0200

    [Storyboard]: Add connection between storyboard and controller

---
+++ Meteo Paris/Meteo Paris/HomePrototypeCell.swift
@@ -0,0 +1,28 @@
+//
+//  HomePrototypeCell.swift
+//  Météo Paris
+//
+//  Created by Thomas VISENTIN on 8/17/17.
+//  Copyright © 2017 Thomas VISENTIN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomePrototypeCell: UITableViewCell {
+    
+    @IBOutlet weak var dayLabelCell: UILabel!
+    @IBOutlet weak var imageViewCell: UIImageView!
+    @IBOutlet weak var degreeLabelCell: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
